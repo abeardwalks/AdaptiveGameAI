21a6369d54842ebf473cccbd81387114b38340c9
Andrew White <scurious.aw@gmail.com>
2013-12-19 22:20:56 +0000

    Started MVC

--- workspace/Nine_Mens_Morris/src/main/TestGameState.java
+++ workspace/Nine_Mens_Morris/src/main/TestGameState.java
@@ -10,27 +10,27 @@ public class TestGameState {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		GameStateInterface gs = new MorrisBoard();
-		System.out.println(gs.getState());
-		
-		System.out.println(gs.addToken('R', 0));
-		System.out.println(gs.getState());
-		
-		System.out.println(gs.addToken('R', 1));
-		
-		System.out.println(gs.addToken('R', 2));
-		
-		System.out.println(gs.addToken('R', 2));
-		
-		System.out.println(gs.moveToken('R', 0, 1));
-		System.out.println(gs.getState());
-		
-		System.out.println(gs.moveToken('R', 0, 9));
-		System.out.println(gs.getState());
-		
-		gs.setState("NBNRRBRBRNBRBRBBRRNBRNBN");
-		gs.setPhase(Phase.TWO);
-		System.out.println("Players can move? Answer: " + gs.playersCanMove());
+//		GameStateInterface gs = new MorrisBoard();
+//		System.out.println(gs.getState());
+//		
+//		System.out.println(gs.addToken('R', 0));
+//		System.out.println(gs.getState());
+//		
+//		System.out.println(gs.addToken('R', 1));
+//		
+//		System.out.println(gs.addToken('R', 2));
+//		
+//		System.out.println(gs.addToken('R', 2));
+//		
+//		System.out.println(gs.moveToken('R', 0, 1));
+//		System.out.println(gs.getState());
+//		
+//		System.out.println(gs.moveToken('R', 0, 9));
+//		System.out.println(gs.getState());
+//		
+//		gs.setState("NBNRRBRBRNBRBRBBRRNBRNBN");
+//		gs.setPhase(Phase.TWO);
+//		System.out.println("Players can move? Answer: " + gs.playersCanMove());
 		
 	}
 
